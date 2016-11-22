31fb7249d80d60c1ecc6cadfbec68c4ec6f104e9
Mecit Semerci <mecit.semerci@gmail.com>
2016-11-22 16:59:27 +0300

    Web and Unit test layers added

---
+++ Medyam.Services.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Medyam.Services.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Medyam.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Medyam.Web.MvcApplication" Language="C#" %>
